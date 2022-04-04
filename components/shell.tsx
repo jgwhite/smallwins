ce8515b51dd9a224d32e8af3aded469e7a209fba
Jamie White <jamie@jgwhite.co.uk>
2022-04-04 15:50:26 +0200

    Clean up Shell

--- components/shell.tsx
+++ components/shell.tsx
@@ -1,52 +1,27 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { FC } from "react";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export default function Example({ children }) {
+const Shell: FC = ({ children }) => {
   return (
-    <>
-      <div className="min-h-full">
-        <div className="bg-white shadow-sm">
-          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              <div className="flex">
-                <div className="flex items-center flex-shrink-0 font-extrabold">
-                  Small Wins
-                </div>
+    <div className="min-h-full">
+      <div className="bg-white shadow-sm">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16">
+            <div className="flex">
+              <div className="flex items-center flex-shrink-0 font-extrabold">
+                Small Wins
               </div>
             </div>
           </div>
         </div>
-
-        <div className="py-10">
-          <main>
-            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
-          </main>
-        </div>
       </div>
-    </>
+
+      <div className="py-10">
+        <main>
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </div>
   );
-}
+};
+
+export default Shell;
